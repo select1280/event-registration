3ffcc1972d69e360ec899e16122c1e0649ace007
peterchi <peterchi@tist.com.tw>
2026-09-08 16:37:26 +0800

    chore: initialize backend

---
+++ event-registration-backend/src/test/java/event_registration/EventRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package event_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
